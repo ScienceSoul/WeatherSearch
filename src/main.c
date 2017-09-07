796ca9fdc8710c346d6dbadc3e00f9b81bcb0c47
Seddik hakime <hakimeseddik@icloud.com>
2017-09-08 00:13:06 +0900

    Add src and include directories.

---
+++ src/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WeatherSearch
+//
+//  Created by Seddik hakime on 07/09/2017.
+//  Copyright © 2017 ScienceSoul. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "grib_api.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
