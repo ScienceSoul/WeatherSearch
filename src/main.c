ff49d23590aa9db02e943eb3a77162fe7b2dd002
Seddik hakime <hakimeseddik@icloud.com>
2017-09-14 02:35:12 +0900

    - Implement so far the code which creates the record data base that the search engine will use.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+
 //
 //  main.c
 //  WeatherSearch
@@ -6,13 +7,37 @@
 //  Copyright © 2017 ScienceSoul. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "CreateRecordDB.h"
 
-#include "grib_api.h"
+#define MAX_VAL_LEN  1024
+
+bool UPDATE_RECORDS = false;
+char **entry_keys = NULL;
+size_t number_keys;
+
+record **db = NULL;
+
+void init(void) {
+    
+    entry_keys = loadSearchableKeys(&number_keys);
+    if (entry_keys == NULL) {
+        fatal(PROGRAM_NAME, "problem loadind the keys definition file.");
+    }
+    fprintf(stdout, "%s: number of keys used to build the data records: %d.\n", PROGRAM_NAME, (int)number_keys);
+    for (int i=0; i<number_keys; i++) {
+        char *str = &entry_keys[i][0];
+        printf("key: %s\n", str);
+    }
+    
+    CreateRecordDB();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    init();
+
+    free(entry_keys);
+    free(db);
     return 0;
 }
